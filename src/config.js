b59cfa2c8d30af0486b4fdbb86621223308564ad
Matt Nguyen <matth3wn@gmail.com>
2019-03-21 13:49:47 -0700

    starting deployment

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/<DATABASE HERE>',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
+  JWT_SECRET: process.env.REACT_APP_JWT_SECRET || 'change-this-secret',
+  JWT_EXPIRY: process.env.REACT_APP_JWT_EXPIRY || '3h'
 }
 // when deploying to heroku you can set the api key with this command
 // heroku config:set API_TOKEN=<YOUR API KEY>
