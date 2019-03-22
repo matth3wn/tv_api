ea31c015a045082774982f8534b14cfadb9465b6
Matt Nguyen <matth3wn@gmail.com>
2019-03-22 07:48:34 -0700

    changed process env name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/<DATABASE HERE>',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/<DATABASE HERE>',
   JWT_SECRET: process.env.REACT_APP_JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.REACT_APP_JWT_EXPIRY || '3h'
 }
